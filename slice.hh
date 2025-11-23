bd6341fce77134ed2b8731d2c7a9324a2b41bd71
bh34e5 <b.hester0614@yahoo.com>
2025-11-23 16:39:46 -0500

    slice constructor and iterators

--- slice.hh
+++ slice.hh
@@ -12,6 +12,12 @@ template <typename T> struct Slice {
         assert(idx < this->len && "Out of bounds access");
         return this->ptr[idx];
     }
+
+    T *begin() { return this->ptr; }
+    T *end() { return this->ptr + this->len; }
+
+    T const *cbegin() { return this->ptr; }
+    T const *cend() { return this->ptr + this->len; }
 };
 
 template<typename T>
@@ -19,3 +25,5 @@ static inline int sliceLen(Slice<T> s) {
     assert(s.len <= INT_MAX && "Slice length larger than max integer");
     return (int) s.len;
 }
+
+#define SLICE(Type, arr) (Slice<Type>{ARRAY_LEN(arr), arr})
